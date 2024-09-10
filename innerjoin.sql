1bdd04f8573f7695b5043244246f707878374058
Heran76 <heran76@gmail.com>
2024-09-10 20:27:20 +0200

    inner join

--- innerjoin.sql
+++ innerjoin.sql
@@ -1,7 +1,16 @@
-SELECT
-s.titulo,
-a.personaje
-FROM series AS s
-INNER JOIN Actuaciones AS a
-ON s.serie_id = a.serie_id
-WHERE titulo = 'The Crown'
+-- Enunciado:
+
+-- Escribe una consulta SQL que te permita obtener el título de la serie, el título de cada episodio y su duración de la serie 'Stranger Things'.
+
+-- El resultado final debe contar con los siguientes nombres de columnas: titulo_serie, titulo_episodio, duracion.
+
+SELECT 
+    s.titulo AS titulo_serie,   -- El título de la serie
+    e.titulo AS titulo_episodio, -- El título del episodio
+    e.duracion AS duracion       -- La duración del episodio
+FROM 
+    Series s
+INNER JOIN 
+    Episodios e ON s.serie_id = e.serie_id -- Unimos las tablas usando el campo serie_id
+WHERE 
+    s.titulo = 'Stranger Things'
